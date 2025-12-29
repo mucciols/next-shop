d74073694c3128879f88a4d1a1622e341461c861
Simone Muccioli <simone.muccioli@consultant.aruba.it>
2025-12-29 20:50:36 +0100

    uldate

---
+++ next-shop/app/pages/Home2b/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+import Title from "@/components/title";
+import { Product } from "@/lib/types";
+
+import Head from "next/head";
+import { useEffect, useState } from "react";
+
+//fetch data client side
+export default function HomePage() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("/api/products");
+      const products = await response.json();
+      setProducts(products);
+    })();
+  }, []);
+
+  console.log("[Home Page] render", products);
+  return (
+    <>
+      <Head>
+        <title>Next Shop clientside 2b</title>
+      </Head>
+      <main className="p-2">
+        <Title>Next Shop clientside 2b</Title>
+        <ul>
+          {products.map((product: Product) => (
+            <li key={product.id}>{product.title}</li>
+          ))}
+        </ul>
+      </main>
+    </>
+  );
+}
