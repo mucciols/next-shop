594476a843041b99e733c4ff8a9fa192c7ad4c86
Simone Muccioli <simone.muccioli@consultant.aruba.it>
2025-12-31 13:35:09 +0100

    spiuk

--- next-shop/components/ProductCard.tsx
+++ next-shop/components/ProductCard.tsx
@@ -1,4 +1,5 @@
 import { Product } from "@/types/product";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function ProductCard({ product }: { product: Product }) {
@@ -6,7 +7,7 @@ export default function ProductCard({ product }: { product: Product }) {
   return (
     <div className="border w-80 shadow hover:shadow-xl">
       <Link href={`/products/${product.id}`}>
-        <img src={product.pictureUrl}  alt="Picture"/>
+        <img width="320" height="240" src={product.pictureUrl}  alt="Picture"/>
         <div className="p-2 flex justify-between items-baseline">
           <h2 className="text-lg font-bold">
             {product.title}
