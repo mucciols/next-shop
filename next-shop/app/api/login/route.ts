999dd040c6fcd52ae4eb1e6a13bdee9856f3842b
Simone Muccioli <simone.muccioli@consultant.aruba.it>
2026-01-01 18:16:09 +0100

    examples

--- next-shop/app/api/login/route.ts
+++ next-shop/app/api/login/route.ts
@@ -1,15 +1,30 @@
 import { NextRequest, NextResponse } from "next/server";
+import { fetchJson } from "../api";
 
 export async function POST(req: NextRequest, res: NextResponse) {
+  const body = await req.json();
 
-  console.log('-------------------------------');
-  console.log('-------------------------------');
-  console.log('-------------------------------');
-  console.log('api/login 1' , req.body);
-  console.log('api/login 2' , res);
-  console.log('-------------------------------');
-  console.log('-------------------------------');
-  console.log('-------------------------------');
+  const { email, password } = body;
 
-  return NextResponse.json(200);
+  if (!email) {
+    return NextResponse.json({ error: "Email requested" }, { status: 400 });
+  }
+
+  if (!password) {
+    return NextResponse.json({ error: "Password requested" }, { status: 400 });
+  }
+
+  try {
+    const { jwt, user } = await fetchJson("http://localhost:1337/auth/local", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ identifier: email, password: password }),
+    });
+    return NextResponse.json({
+      id: user.id,
+      name: user.username,
+    });
+  } catch (error) {
+    return NextResponse.json({ error: error }, { status: 401 });
+  }
 }
