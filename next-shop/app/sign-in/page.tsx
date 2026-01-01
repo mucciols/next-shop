73b7d8659b8c79375d5e363b95875222752383a8
Simone Muccioli <simone.muccioli@consultant.aruba.it>
2026-01-01 14:01:32 +0100

    tett

--- next-shop/app/sign-in/page.tsx
+++ next-shop/app/sign-in/page.tsx
@@ -1,20 +1,30 @@
+'use client'
 import Button from "@/components/Button";
 import Field from "@/components/Field";
 import Input from "@/components/Input";
 import Page from "@/components/Page";
+import { useState } from "react";
 
 export default function SignInPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log('event', { email, password });
+  }
+
   return (
     <Page title="sign in">
-      <form>
+      <form onSubmit={handleSubmit}>
         <Field label="Email">
-          <Input type="email"></Input>
+          <Input required onChange={(event) => setEmail(event.target.value)} value={email} type="email"></Input>
         </Field>
 
         <Field label="Password">
-          <Input type="password"></Input>
+          <Input required onChange={(event) => setPassword(event.target.value)} value={password} type="password"></Input>
         </Field>
-        <Button>
+        <Button type="submit">
           Sign In
         </Button>
       </form>
--- next-shop/components/Input.tsx
+++ next-shop/components/Input.tsx
@@ -1,6 +1,11 @@
-export default function Input({type}:{type:string}) {
+import { ChangeEventHandler } from "react";
+
+export default function Input({type, value, onChange, required}
+                              :{type:string, value: string, 
+                                onChange:ChangeEventHandler<HTMLInputElement>, required?: boolean}) {
   return (
-    <input type={type} 
+    <input type={type}  value={value} onChange={onChange}
+      {...(required ? { required: true } : {})} // aggiunge solo se true
       className="border px-3 py-1 w-80"
     />
   )
